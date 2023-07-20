87d6e98f5953abf414df9f37b9c93f9bcf8bb1bf
Muhammad Adnan <daniadnan0349@gmail.com>
2023-07-20 12:53:59 +0500

    Build changes

--- enatega-multivendor-app/environment.js
+++ enatega-multivendor-app/environment.js
@@ -9,72 +9,71 @@ const ENV = {
     // GRAPHQL_URL: 'http://10.97.28.129:8001/graphql',
     // WS_GRAPHQL_URL: 'ws://10.97.28.129:8001/graphql',
     // SERVER_URL: 'http://10.97.28.129:8001/', // put / at the end of server url
-    GRAPHQL_URL: 'https://enatega-multivendor.up.railway.app/graphql',
-    WS_GRAPHQL_URL: 'wss://enatega-multivendor.up.railway.app/graphql',
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/', // put / at the end of server url
+    GRAPHQL_URL: 'https://shajerapi.up.railway.app/graphql',
+    WS_GRAPHQL_URL: 'wss://shajerapi.up.railway.app/graphql',
+    SERVER_URL: 'https://shajerapi.up.railway.app/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '967541328677-uq7f7odvmeea2pb2sq0l7q320ds86536.apps.googleusercontent.com',
+      '13794450669-6k0l8vgflotmg1llvl037ptu2kshaau4.apps.googleusercontent.com',
     ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-u9lbhmiesp67j3md9b8nk6mkhooeljur.apps.googleusercontent.com',
-    AMPLITUDE_API_KEY: '2114f5db4c014dc7ad4ed2ad747341b5',
-    GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
+      '13794450669-s3iselmrkivsu9jcipqcf3q3ulvcr3e6.apps.googleusercontent.com',
+    AMPLITUDE_API_KEY: '58dcfee9467cc4c3c616b856444b786e',
+    GOOGLE_MAPS_KEY: 'AIzaSyCX74aTKx5CEV66XhXaDeV6HkaoTwMRzoo',
     EXPO_CLIENT_ID:
-      '630195385603-82e52jpb722a8l0huhkspq3tqh2d6r6f.apps.googleusercontent.com',
+      '13794450669-qors86fskm3khc9ipl5mbca3el79foki.apps.googleusercontent.com',
     SENTRY_DSN:
-      'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
+      'https://0a25963153144c7c8792cc0887cc5b85@o4505548794757120.ingest.sentry.io/4505559937712128',
     TERMS_AND_CONDITIONS: 'https://enatega.ninjascode.com/privacy-policy',
     PRIVACY_POLICY: 'https://enatega.ninjascode.com/privacy-policy',
     STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
-
     STRIPE_IMAGE_URL:
       'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
-    STRIPE_STORE_NAME: 'Enatega',
+    STRIPE_STORE_NAME: 'shajer',
     TEST_OTP: '111111',
     GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
   },
   staging: {
-    GRAPHQL_URL: 'https://enatega-multivendor.up.railway.app/graphql',
-    WS_GRAPHQL_URL: 'wss://enatega-multivendor.up.railway.app/graphql',
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/', // put / at the end of server url
+    GRAPHQL_URL: 'https://shajerapi.up.railway.app/graphql',
+    WS_GRAPHQL_URL: 'wss://shajerapi.up.railway.app/graphql',
+    SERVER_URL: 'https://shajerapi.up.railway.app/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '967541328677-nf8h4ou7rhmq9fahs87p057rggo95eah.apps.googleusercontent.com',
+      '13794450669-6k0l8vgflotmg1llvl037ptu2kshaau4.apps.googleusercontent.com',
     ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
-    AMPLITUDE_API_KEY: '42b182f3d18a21e8ab0ae881c6ef475f',
-    GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
+      '13794450669-s3iselmrkivsu9jcipqcf3q3ulvcr3e6.apps.googleusercontent.com',
+    AMPLITUDE_API_KEY: '58dcfee9467cc4c3c616b856444b786e',
+    GOOGLE_MAPS_KEY: 'AIzaSyCX74aTKx5CEV66XhXaDeV6HkaoTwMRzoo',
     EXPO_CLIENT_ID:
-      '967541328677-d46sl62t52g5r3o5m0mnl2hpptr242nl.apps.googleusercontent.com',
+      '13794450669-qors86fskm3khc9ipl5mbca3el79foki.apps.googleusercontent.com',
     SENTRY_DSN:
-      'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
+      'https://0a25963153144c7c8792cc0887cc5b85@o4505548794757120.ingest.sentry.io/4505559937712128',
     TERMS_AND_CONDITIONS: 'https://enatega.ninjascode.com/privacy-policy',
     PRIVACY_POLICY: 'https://enatega.ninjascode.com/privacy-policy',
     STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
     STRIPE_IMAGE_URL:
       'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
-    STRIPE_STORE_NAME: 'Enatega',
+    STRIPE_STORE_NAME: 'shajer',
     TEST_OTP: '111111',
     GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
   },
   production: {
-    GRAPHQL_URL: 'https://enatega-multivendor.up.railway.app/graphql',
-    WS_GRAPHQL_URL: 'wss://enatega-multivendor.up.railway.app/graphql',
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/', // put / at the end of server url
+    GRAPHQL_URL: 'https://shajerapi.up.railway.app/graphql',
+    WS_GRAPHQL_URL: 'wss://shajerapi.up.railway.app/graphql',
+    SERVER_URL: 'https://shajerapi.up.railway.app/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '967541328677-nf8h4ou7rhmq9fahs87p057rggo95eah.apps.googleusercontent.com',
+      '13794450669-6k0l8vgflotmg1llvl037ptu2kshaau4.apps.googleusercontent.com',
     ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
-    AMPLITUDE_API_KEY: '42b182f3d18a21e8ab0ae881c6ef475f',
-    GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
+      '13794450669-s3iselmrkivsu9jcipqcf3q3ulvcr3e6.apps.googleusercontent.com',
+    AMPLITUDE_API_KEY: '58dcfee9467cc4c3c616b856444b786e',
+    GOOGLE_MAPS_KEY: 'AIzaSyCX74aTKx5CEV66XhXaDeV6HkaoTwMRzoo',
     EXPO_CLIENT_ID:
-      '967541328677-d46sl62t52g5r3o5m0mnl2hpptr242nl.apps.googleusercontent.com',
+      '13794450669-qors86fskm3khc9ipl5mbca3el79foki.apps.googleusercontent.com',
     SENTRY_DSN:
-      'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
+      'https://0a25963153144c7c8792cc0887cc5b85@o4505548794757120.ingest.sentry.io/4505559937712128',
     TERMS_AND_CONDITIONS: 'https://enatega.ninjascode.com/privacy-policy',
     PRIVACY_POLICY: 'https://enatega.ninjascode.com/privacy-policy',
     STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
     STRIPE_IMAGE_URL:
       'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
-    STRIPE_STORE_NAME: 'Enatega',
+    STRIPE_STORE_NAME: 'shajer',
     TEST_OTP: '111111',
     GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
   }
